cdb1efd18ae26144f5bba8192f4e23fb1fa2732b
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-08-23 00:39:04 +0200

    :recycle: Refactor(controller): remove useless try/catch

--- api/app/Controllers/Http/CookiesController.ts
+++ api/app/Controllers/Http/CookiesController.ts
@@ -3,17 +3,17 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { DateTime } from 'luxon'
 
 export default class CookiesController {
-	public async show({ response, params }: HttpContextContract) {
+	public async show({ params }: HttpContextContract) {
 		const cookie = await Cookie.findBy('ip', params.ip)
-		return response.status(200).send({ cookie })
+		return { cookie }
 	}
 
-	public async store({ response, request }: HttpContextContract) {
+	public async store({ request }: HttpContextContract) {
 		const date = new Date()
 		const ip = request.input('ip')
 		const expiration = DateTime.fromJSDate(new Date(date.setMonth(date.getMonth() + 6)))
 		await Cookie.updateOrCreate({ ip }, { ip, expiration })
 
-		return response.status(201).send({ message: 'Cookies are allow for user : ' + ip })
+		return { message: 'Cookies are allow for user : ' + ip }
 	}
 }
