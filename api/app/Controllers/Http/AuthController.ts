9bd2245431465744783370182df9378dd1fa3212
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-08-23 00:25:34 +0200

    :recycle: Refactor(controller): remove useless try/catch

--- api/app/Controllers/Http/AuthController.ts
+++ api/app/Controllers/Http/AuthController.ts
@@ -5,42 +5,29 @@ export default class AuthController {
 		const email = request.input('email')
 		const password = request.input('password')
 		const remember_me = !!request.input('remember_me')
-		try {
-			await auth.attempt(email, password, remember_me)
-			return { user: auth.user }
-		} catch (error) {
-			if (error.code === 'E_INVALID_AUTH_UID') return { error: "L'utilisateur n'a pas été trouvé" }
-			if (error.code === 'E_INVALID_AUTH_PASSWORD') return { error: "L'identifiant ou le mot de passe est incorrecte" }
-		}
+
+		await auth.attempt(email, password, remember_me)
+		return { user: auth.user }
 	}
 
 	public async logoutWeb({ auth }: HttpContextContract) {
-		try {
-			await auth.logout()
-			return { message: 'Vous avez été déconnecté' }
-		} catch (error) {}
+		await auth.logout()
+		return { message: 'Vous avez été déconnecté' }
 	}
 
 	public async loginApi({ request, auth }: HttpContextContract) {
 		const email = request.input('email')
 		const password = request.input('password')
 
-		try {
-			const token = await auth.use('api').attempt(email, password, {
-				expiresIn: '10 days'
-			})
-			return token.toJSON()
-		} catch (error) {
-			if (error.code === 'E_INVALID_AUTH_UID') return { error: "L'utilisateur n'a pas été trouvé" }
-			if (error.code === 'E_INVALID_AUTH_PASSWORD') return { error: "L'identifiant ou le mot de passe est incorrecte" }
-		}
+		const token = await auth.use('api').attempt(email, password, {
+			expiresIn: '10 days'
+		})
+		return { token: token.toJSON() }
 	}
 
 	public async logoutApi({ auth }: HttpContextContract) {
-		try {
-			await auth.use('api').logout()
-			return { message: 'Vous avez été déconnecté' }
-		} catch (error) {}
+		await auth.use('api').logout()
+		return { message: 'Vous avez été déconnecté' }
 	}
 
 	public async user({ auth }: HttpContextContract) {
