9d2e999fbc3c88b29221e90af5ad20dfdfed54bd
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-08-23 00:45:16 +0200

    :recycle: Refactor(configuration): remove env var

--- frontend/configurations/Env.ts
+++ frontend/configurations/Env.ts
@@ -1,6 +1,1 @@
-export default {
-	baseUrl:
-		process.env.NODE_ENV === 'developpement'
-			? 'http://localhost:3333/' // developpement
-			: 'http://localhost:3333/', // production
-}
+export default {}
