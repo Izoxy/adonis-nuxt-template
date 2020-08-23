07e46e2fda09a02a811665bc27d2d95e735bae25
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-08-23 02:18:41 +0200

    :sparkles: feature(frontend): add dir options

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -34,6 +34,15 @@ export default {
 	 */
 	srcDir: 'src',
 
+	dir: {
+		assets: 'assets',
+		layouts: 'template/layouts',
+		middleware: 'middleware',
+		pages: 'template/pages',
+		static: 'public',
+		store: 'store',
+	},
+
 	/*
 	 ** Nuxt.js modules
 	 ** See ~/configurations/*
