aca5d1cca0e762113e9f32a4d884ea89a60efe4c
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-09-23 14:02:09 +0200

    :construction: work(translation): fix

--- client/src/locales/en-US.ts
+++ client/src/locales/en-US.ts
@@ -1,5 +1,5 @@
 export default {
 	welcome: 'Welcome to <span style="color: #5a45ff;">Adonis</span>, <span style="color: #108775;">Nuxt</span> app !',
-	headerLangs: 'Switch lanagage to',
+	headerLangs: 'Switch langage to',
 	description: 'Create a fast, efficient and powerful application thanks to this model combining the best combination of technologies',
 }
