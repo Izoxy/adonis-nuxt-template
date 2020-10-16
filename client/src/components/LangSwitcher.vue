7a31a0dfa4bb18953f7c1a96937fb646bb1e7271
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-10-16 12:34:05 +0200

    :construction: work(translations): upgrade option api to composition api

--- client/src/components/LangSwitcher.vue
+++ client/src/components/LangSwitcher.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="switcher-container">
-		<div style="display: inline-flex;" :text="this.$i18n.locale.toUpperCase()" variant="primary" class="m-2">
+		<div style="display: inline-flex;" :text="$i18n.locale.toUpperCase()" variant="primary" class="m-2">
 			<div id="dropdown-header-label">{{ $t('headerLangs') }}</div>
 			<div style="margin-left: 5px; cursor: pointer;" v-for="(lang, key) in availableLocales" :key="key" @click.prevent="changeLanguage(lang.code)">{{ lang.code.toUpperCase() }}</div>
 		</div>
@@ -8,17 +8,20 @@
 </template>
 
 <script>
+import { useContext, computed } from '@nuxtjs/composition-api'
+
 export default {
-	name: 'LangSwitcher',
-	computed: {
-		availableLocales() {
-			return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
-		},
-	},
-	methods: {
-		changeLanguage(lang) {
-			this.$i18n.setLocale(lang)
-		},
+	setup() {
+		const { store } = useContext()
+		const availableLocales = computed(() => store.$i18n.locales.filter((i) => i.code != store.$i18n.locale))
+		function changeLanguage(lang) {
+			store.$i18n.setLocale(lang)
+		}
+
+		return {
+			availableLocales,
+			changeLanguage,
+		}
 	},
 }
 </script>
